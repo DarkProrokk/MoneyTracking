05e661bc6d99fb8b834fc8d67215f522d55c0746
DarkProrok <Seroshtanmakcs@gmail.com>
2024-04-07 22:11:20 +1000

    add CreateItemViewModel for creating new item(just model and test this, no functionality)

---
+++ MoneyTracking.Mvc/Models/CreateItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿using DataLayer.Entity;
+namespace MoneyTracking.Models;
+
+
+public class CreateItemViewModel
+{
+    public Item Item { get; set; }
+    public IEnumerable<Tag> Tags { get; set; }
+}
