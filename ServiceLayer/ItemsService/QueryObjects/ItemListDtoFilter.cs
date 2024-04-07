546b34812ad0b761295a5fdd97bfe0622f516fc1
DarkProrok <Seroshtanmakcs@gmail.com>
2024-04-07 22:12:49 +1000

    Started working with item filters

---
+++ ServiceLayer/ItemsService/QueryObjects/ItemListDtoFilter.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceLayer.ItemsService.QueryObjects;
+
+public enum ItemsFilterBy
+{
+    [Display(Name = "All")] NoFilter = 0,
+    [Display(Name = "По цене..")] ByPrice,
+    [Display(Name = "По тегам...")] ByTags,
+    [Display(Name = "По полезности... ")] ByUseFul
+}
+
+public static class ItemListDtoFilter
+{
+    public static IQueryable<ItemsListDto> FilterItemsBy(
+        this IQueryable<ItemsListDto> items,
+        ItemsFilterBy filterBy,
+        string filterValue)
+    {
+        if (string.IsNullOrEmpty(filterValue))
+            return items;
+        switch (filterBy)
+        {
+            case ItemsFilterBy.NoFilter:
+                return items;
+            case ItemsFilterBy.ByPrice:
+                return items;
+            case ItemsFilterBy.ByTags:
+                return items;
+            case ItemsFilterBy.ByUseFul:
+                return items;
+            default:
+                throw new ArgumentOutOfRangeException(
+                    nameof(filterBy), filterBy, null);
+        }
+    }
+}
